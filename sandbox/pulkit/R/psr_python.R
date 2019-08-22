385a5866da104e24b9b62b7f1f812ef9b414d3e6
sen-saven <tasosgribas@hotmail.com>
2019-08-22 22:49:08 +0200

    Delete psr_python.R

--- sandbox/pulkit/R/psr_python.R
+++
@@ -1,6 +0,0 @@
-#' @name psr_python
-#' @title Data to test Probabilistic Sharpe Ratio
-#' @description This data set will be used to validate the results of the paper
-#' @docType data
-#' @usage psr_python
-NULL
